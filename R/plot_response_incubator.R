a4542b7c25a5de70051b12d753427f06426b478e
Rainer Krug <Rainer@krugs.de>
2021-09-23 12:39:09 +0200

    Fix x-scales response_incubator plot

--- R/plot_response_incubator.R
+++ R/plot_response_incubator.R
@@ -74,7 +74,7 @@ plot_response_incubator <- function(
     ) +
     ggplot2::geom_point() +
     ggplot2::xlab("") +
-    ggplot2::facet_wrap(~mxs, ncol = 3, scales = "free",) +
+    ggplot2::facet_wrap(~mxs, ncol = 3, scales = "free_y",) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45))
   p
 }
