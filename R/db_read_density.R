5cf907d8cd6d4d72aedd0685535e16b3cc78383a
Rainer M. Krug <Rainer@krugs.de>
2021-10-21 11:41:49 +0200

    0.1.23: Add from_timestamp and to_timestamp arguments to db_read_... functions

--- R/db_read_density.R
+++ R/db_read_density.R
@@ -7,6 +7,8 @@
 #' To fetch the actual data, execute \code{collect()}.
 #' @param db fully qualified path to the sqlite database. Default, read from option \code{RRDdb}.
 #'   If not set, defaults to option \code{RRDdb}; if this is not set, defaults to \code{LEEF.RRD.sqlite}
+#' @param from_timestamp \code{integer}. Earliest timestamp to return. Default: \code{20210920}
+#' @param to_timestamp \code{integer}. Latest timestamp to return. Default: \code{21000101}
 #'
 #' @return Link to the table \code{density} in the RRD to be used with \code{dplyr} and friends
 #'
@@ -18,10 +20,14 @@
 #'
 #' @examples
 db_read_density <- function(
-  db = getOption("RRDdb", "LEEF.RRD.sqlite")
+  db = getOption("RRDdb", "LEEF.RRD.sqlite"),
+  from_timestamp = 20210920,
+  to_timestamp = 21000101
 ){
   con <- DBI::dbConnect(RSQLite::SQLite(), db, flags = RSQLite::SQLITE_RO)
   data <- con %>%
-    dplyr::tbl("density")
+    dplyr::tbl("density") %>%
+    filter (as.integer(timestamp) >= as.integer(from_timestamp)) %>%
+    filter (as.integer(timestamp) <= as.integer(to_timestamp))
   return(data)
 }
