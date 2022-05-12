6770118b1f8823c2577d740bf588028c3c5e59b3
Rainer M Krug <Rainer@krugs.de>
2022-05-12 14:25:05 +0200

    v0.1.16:Extend add_experiment_table for immigration and light schedule and add functions to read these

--- R/add_experiment_tables.R
+++ R/add_experiment_tables.R
@@ -3,7 +3,9 @@
 #' @param db fully qualified path to the sqlite database. Default, read from option \code{RRDdb}.
 #'   If not set, defaults to option \code{RRDdb}; if this is not set, defaults to \code{LEEF.RRD.sqlite}
 #' @param composition fully qualified path to the \code{csv} file with the composition data
-#' @param experimetal_design fully qualified path to the \code{csv} file with the experimetal_design data
+#' @param composition fully qualified path to the \code{csv} file with the composition data
+#' @param light_decline_schedule fully qualified path to the \code{csv} file with the light decline dates
+#' @param immigration_schedule fully qualified path to the \code{csv} file with the immigration event dates
 #' @param overwrite if \code{TRUE}, the existing tables will be overwritten. There is actually no need for this.
 #'
 #' @importFrom utils read.csv
@@ -15,45 +17,35 @@ add_experiment_tables <- function(
   db = getOption("RRDdb", "LEEF.RRD.sqlite"),
   composition = NULL,
   experimetal_design = NULL,
+  light_decline_schedule = NULL,
+  immigration_schedule = NULL,
   overwrite = FALSE
 ) {
+
+  addtbl <- function(con, csv, tbl){
+    if (!is.null(csv)) {
+      if (!file.exists(csv)) {
+        stop("'", csv, "'ad_ must point to an existing csv file!")
+      }
+      DBI::dbWriteTable(
+        con,
+        name = tbl,
+        value = utils::read.csv(csv),
+        overwrite = overwrite,
+        append = FALSE
+      )
+    }
+  }
+
   con <- NULL
   con <- DBI::dbConnect(RSQLite::SQLite(), db)
   on.exit({
     try(DBI::dbDisconnect(con), silent = TRUE)
   })
 
-  if (!is.null(composition)) {
-    if (!file.exists(composition)) {
-      stop("'compsition'ad_ must poiint to an existing csv file!")
-    }
-#    if (!DBI::dbExistsTable(con, "composition")) {
-      DBI::dbWriteTable(
-        con,
-        name = "composition",
-        value = utils::read.csv(composition),
-        overwrite = overwrite,
-        append = FALSE
-      )
-#    } else {
-#      message("Table 'composition' exists in database. To overwrite specify 'overwrite = TRUE'")
-#    }
-  }
+  addtbl(con, composition, "composition")
+  addtbl(con, experimetal_design, "experimetal_design")
+  addtbl(con, light_decline_schedule, "light_decline_schedule")
+  addtbl(con, immigration_schedule, "immigration_schedule")
 
-  if (!is.null(experimetal_design)) {
-    if (!file.exists(experimetal_design)) {
-      stop("'experimetal_design' must poiint to an existing csv file!")
-    }
-#    if (!DBI::dbExistsTable(con, "experimetal_design")) {
-      DBI::dbWriteTable(
-        con,
-        name = "experimetal_design",
-        value = utils::read.csv(experimetal_design),
-        overwrite = overwrite,
-        append = FALSE
-      )
-#    } else {
-#      message("Table 'experimetal_design' exists in database. To overwrite specify 'overwrite = TRUE'")
-#    }
-  }
 }
---
+++ R/db_read_light_decline_schedule.R
@@ -0,0 +1,26 @@
+#' Read \code{light_decline_schedule} from RRD database and return as an \code{tbl_SQLiteConnection}.
+#'
+#' This function does not actually fetch the data,
+#' but returns an \code{tbl_SQLiteConnection} object which can be further processed / filtered
+#' using \code{dplyr}.
+#' See \link{https://db.rstudio.com/r-packages/dplyr/} for details.
+#' To fetch the actual data, execute \code{collect()}.
+#' @param db fully qualified path to the sqlite database. Default, read from option \code{RRDdb}.
+#'   If not set, defaults to option \code{RRDdb}; if this is not set, defaults to \code{LEEF.RRD.sqlite}
+#'
+#' @return Link to the table \code{density} in the RRD to be used with \code{dplyr} and friends
+#'
+#' @importFrom DBI dbConnect
+#' @importFrom RSQLite SQLite SQLITE_RO
+#' @importFrom dplyr tbl
+#'
+#' @export
+#'
+#' @examples
+db_read_light_decline_schedule <- function(
+  db = getOption("RRDdb", "LEEF.RRD.sqlite")
+){
+  con <- DBI::dbConnect(RSQLite::SQLite(), db, flags = RSQLite::SQLITE_RO)
+  data <- dplyr::tbl(con, "light_decline_schedule")
+  return(data)
+}
