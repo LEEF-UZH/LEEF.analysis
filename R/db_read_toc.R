b3d76eb9abdfcc7b0be37e340a55c3237d65c7d0
Rainer M. Krug <Rainer@krugs.de>
2022-09-30 15:01:49 +0200

    v0.3.3: Fixed plot and db_read_toc function

--- R/db_read_toc.R
+++ R/db_read_toc.R
@@ -9,6 +9,8 @@
 #'   If not set, defaults to option \code{RRDdb}; if this is not set, defaults to \code{LEEF.RRD.sqlite}
 #' @param from_timestamp \code{integer}. Earliest timestamp to return. Default: \code{20210920}
 #' @param to_timestamp \code{integer}. Latest timestamp to return. Default: \code{21000101}
+#' @param duplicates function which will be used to combine duplicates. If \code{NULL}, co summarising is done.
+#'   Default is `mean`.
 #'
 #' @return Link to the table \code{density} in the RRD to be used with \code{dplyr} and friends
 #'
@@ -22,12 +24,19 @@
 db_read_toc <- function(
   db = getOption("RRDdb", "LEEF.RRD.sqlite"),
   from_timestamp = 20210920,
-  to_timestamp = 21000101
+  to_timestamp = 21000101,
+  duplicates = mean
 ){
   con <- DBI::dbConnect(RSQLite::SQLite(), db, flags = RSQLite::SQLITE_RO)
   data <- con %>%
     dplyr::tbl("toc") %>%
     filter (as.integer(timestamp) >= as.integer(from_timestamp)) %>%
     filter (as.integer(timestamp) <= as.integer(to_timestamp))
+
+  if (!is.null(duplicates)) {
+    data <- data %>%
+      group_by(timestamp, day, type, bottle, temperature, richness, composition, incubator) %>%
+      summarise(concentration = mean(concentration), cv = NA, n = n())
+  }
   return(data)
 }
