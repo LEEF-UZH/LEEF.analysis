8fbf354c7349b87069828811ca93b84f8419354a
Rainer M Krug <Rainer@krugs.de>
2022-10-04 09:07:42 +0200

    v0.3.5: add `make_views()` function to make all views

---
+++ R/make_views.R
@@ -0,0 +1,24 @@
+#' Create all views
+#'
+#' This function calls the other \code{make_view_...()} functions, wrapped in individual \code{try()} blocks.
+#' @param db fully qualified path to the sqlite database. Default, read from option \code{RRDdb}.
+#'   If not set, defaults to option \code{RRDdb}; if this is not set, defaults to \code{LEEF.RRD.sqlite}
+#' @param start_date the start date of the experiment
+#' @param overwrite if \code{TRUE}, overwrite existing view
+#'
+#' @return invisibly \code{NULL}
+#'
+#' @export
+#'
+#' @examples
+make_views <- function(
+  db = getOption("RRDdb", "LEEF.RRD.sqlite"),
+  start_date = "2021-09-20",
+  overwrite = FALSE
+){
+  try(make_view_density(db = db, start_date = start_date, overwrite = overwrite))
+  try(make_view_light_decline(db = db, start_date = start_date, overwrite = overwrite))
+  try(make_view_o2(db = db, start_date = start_date, overwrite = overwrite))
+  try(make_view_toc(db = db, start_date = start_date, overwrite = overwrite))
+  invisible(NULL)
+}
