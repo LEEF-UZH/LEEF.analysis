3dd7b48ca3230c7d033fd3cc705a4a451e8b6a7c
Rainer M Krug <Rainer@krugs.de>
2023-02-07 15:49:04 +0100

    Intermediate

---
+++ R/gate_flowcytometer_files.R
@@ -0,0 +1,54 @@
+#' Classify algae_traits data.frame
+#'
+#' @param datadir `character` vector containing the root directory for all files
+#' @param bemovi_extract_name `character` vector containing the name of the
+#'   bemovi config file including path.
+#' @param classifier_constant_name `character` vector of name of the classifier
+#'   for temperature treatment **constant** including path
+#' @param classifier_increasing_name `character` vector of name of the
+#'   classifier for temperature treatment **increasing** including path
+#' @return `list` containing three objects:
+#'       - `mean_density_per_ml`
+#'       - `morph_mvt`
+#'       - `trajectory_data`
+#'
+#' @importFrom yaml read_yaml
+#' @importFrom utils read.table
+#'
+#' @export
+#'
+#' @md
+#' @examples
+#'
+#'
+classify_bemovi_files <- function(
+  datadir,
+  bemovi_extract_name = NULL,
+  classifier_constant_name,
+  classifier_increasing_name
+){
+
+  p <- yaml::read_yaml(bemovi_extract_name)
+
+
+# The classification ------------------------------------------------------
+
+  morph_mvt <- readRDS(file.path(datadir, p$merged.data.folder, p$morph_mvt))
+  morph_mvt <- morph_mvt[, grep("_prob|species", names(morph_mvt), invert = TRUE)]
+
+  classified <- LEEF.measurement.bemovi::classify(
+    bemovi_extract = bemovi_extract_name,
+    morph_mvt = morph_mvt,
+    trajectory_data = readRDS(file.path(datadir, p$merged.data.folder, p$master)),
+    classifiers_constant = readRDS(classifier_constant_name),
+    classifiers_increasing = readRDS(classifier_increasing_name),
+    video_description_file = as.data.frame(read.table(file.path(datadir, p$video.description.folder, p$video.description.file), sep = "\t", header = TRUE, stringsAsFactors = FALSE)),
+    composition = utils::read.csv(file.path(datadir, "compositions.csv"))
+  )
+
+
+# Return the classifications --------------------------------------------------
+
+
+  return(classified)
+}
