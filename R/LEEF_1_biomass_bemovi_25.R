0f0fc33b84cda5e8e762cbcfcce0dd20f36b0fe2
Rainer M. Krug <Rainer@krugs.de>
2023-05-22 14:26:01 +0200

    First step biomass

---
+++ R/LEEF_1_biomass_bemovi_25.R
@@ -0,0 +1,94 @@
+#' LEEF-1 - Add biomass to traits
+#'
+#' @param ciliate_traits_25 traits as read from file \code{morph_mvt_TIMESTAMP.rds}
+#' @param ciliate_density_25 density as read from file \code{mean_density_per_ml_TIMESTAMP.rds}
+#'
+#' @return list containing two objects, \code{traits} containing complete traits file
+#'   as the argument \code{algai_traits} day includinc biomass column and \code{biomasses}
+#'   per timestamp, bottle and species per milliliter.
+#' @export
+#'
+#' @examples
+LEEF_1_biomass_bemovi_25 <- function(
+    ciliate_traits_25,
+    ciliate_density_25
+){
+
+  video_biomass_species <- c("Paramecium_bursaria","Paramecium_caudatum","Coleps_irchel", # species for which biomass is calculated
+                             "Stylonychia1","Stylonychia2","Colpidium","Euplotes",
+                             "Tetrahymena", "Loxocephallus",
+                             "Dexiostoma")
+
+  # 25x
+
+  ciliate_traits_25_normalCases <- ciliate_traits_25 %>%
+    dplyr::filter(species %in% video_biomass_species) %>%
+    mutate(
+      height = ifelse(
+        species %in% c("Euplotes","Stylonychia1","Stylonychia2"),
+        mean_minor/3,
+        ifelse(
+          species %in% c("Paramecium_bursaria"),
+          mean_minor/1.5,
+          mean_minor)
+      ),
+      biomass = (4/3) * pi * (mean_minor/2) * (height/2) * (mean_major/2), # calculate biomass
+      biomass = biomass/10^12  # change it from um3 to g, assuming water density
+    )
+
+  ciliate_traits_25_NotBiomass <- ciliate_traits_25 %>%
+    dplyr::filter(!(species %in% video_biomass_species)) %>%
+    mutate(
+      height=as.numeric(NA),
+      biomass=as.numeric(NA)
+    )
+
+
+  # join datasets again
+
+
+  ciliate_traits_25 <- rbind(
+    ciliate_traits_25_normalCases,
+    ciliate_traits_25_NotBiomass
+  ) # traits dataset finished here
+
+  # calculate biomass per milliliter
+
+  extrapolation.factor_25 <- 23.367
+
+
+  biomasses <- ciliate_traits_25 %>%
+    group_by(timestamp, bottle, species) %>%
+    summarize(biomass = sum(biomass, na.rm = T)) %>%
+    mutate(
+      biomass = biomass * extrapolation.factor_25,
+      biomass = ifelse(
+        !(species %in% video_biomass_species),
+        NA,
+        biomass
+      )
+    ) # add biomass=NA if not a species
+
+
+  biomasses$timestamp <- as.character(biomasses$timestamp)
+
+  densities <- full_join(
+    ciliate_density_25,
+    biomasses,
+    by = c("timestamp", "bottle", "species")
+  )
+  densities$biomass[densities$species %in% video_biomass_species & is.na(densities$biomass)] <- 0
+    # mutate(
+    #   biomass = case_when(
+    #     species %in% flowcam_biomass_species & is.na(biomass) ~ 0,
+    #     TRUE ~ biomass)
+    # )
+
+  return(
+    list(
+      traits = ciliate_traits_25,
+      density = densities
+    )
+  )
+
+}
---
+++ R/LEEF_1_biomass_flowcam.R
@@ -0,0 +1,138 @@
+#' LEEF-1 - Add biomass to traits
+#'
+#' @param algae_traits algae traits as read from file \code{algae_traits_TIMESTAMP.rds}
+#' @param algae_density algae density as read from file \code{algae_density_TIMESTAMP.rds}
+#'
+#' @return list containing two objects, \code{traits} containing complete traits file
+#'   as the argument \code{algai_traits} day includinc biomass column and \code{biomasses}
+#'   per timestamp, bottle and species per milliliter.
+#' @export
+#'
+#' @examples
+LEEF_1_biomass_flowcam <- function(
+    algae_traits,
+    algae_density
+){
+
+  algae_traits <- algae_traits %>%
+    dplyr::filter(!(species %in% c("Chlamydomonas","Small_cells") & area_abd >500)) %>% # filter out individuals that are too big for these two classes
+    collect()
+
+  flowcam_biomass_species <- c("Chlamydomonas","ChlamydomonasClumps","DividingChlamydomonas", # species for which biomass is calculated
+                               "Cosmarium","Desmodesmus","DesmodesmusClumps",
+                               "Monoraphidium", "Small_cells",
+                               "Staurastrum1", "Staurastrum2")
+
+  # Median Chlamy and Desmo based on classifier data
+
+  ChlamyMedianWidth <- 5.8
+  DesmoMedianWidth <- 7.5
+
+  # Biomass calculation of normal cases
+
+  algae_traits_normalCases <- algae_traits %>%
+    dplyr::filter(species %in% c("Chlamydomonas",
+                                 "Desmodesmus",
+                                 "Small_cells")) %>%
+    mutate(height = ifelse(species == "Desmodesmus", width/4,  width),
+           biomass=(4/3)*pi*(width/2)*(height/2)*(length/2), # calculate biomass
+           biomass=biomass/10^12)  # change it from um3 to g, assuming water density
+  # Special cases
+
+  ## 2 ellipsoids
+
+  algae_traits_2ellipsoids <- algae_traits %>%
+    dplyr::filter(species %in% c("DividingChlamydomonas",
+                                 "Cosmarium",
+                                 "Staurastrum2")) %>%
+    mutate(height = length/4,
+           biomass=2 * (4/3)*pi*(width/2)*height*(length/4), # calculate biomass
+           biomass=biomass/10^12)  # change it from um3 to g, assuming water density
+
+  ## 2 truncated cones
+
+  algae_traits_2cones <- algae_traits %>%
+    dplyr::filter(species %in% c("Staurastrum1")) %>%
+    mutate(height = length/2,
+           biomass=2 * (1/3)*pi*(width/2)*((length/6)^2 + (length/6)*(length/2) + (length/2)^2), # calculate biomass
+           biomass=biomass/10^12)  # change it from um3 to g, assuming water density
+
+  ## ChlamyClumps
+
+  algae_traitsChlamyClumps <- algae_traits %>%
+    dplyr::filter(species %in% c("ChlamydomonasClumps")) %>%
+    mutate(height=ChlamyMedianWidth,
+           biomass=area_abd*height, # calculate biomass
+           biomass=biomass/10^12)  # change it from um3 to g, assuming water density
+
+  ## DesmoClumps
+
+  algae_traitsDesmoClumps <- algae_traits %>%
+    dplyr::filter(species %in% c("DesmodesmusClumps")) %>%
+    mutate(height=DesmoMedianWidth,
+           biomass=area_abd*height, # calculate biomass
+           biomass=biomass/10^12)  # change it from um3 to g, assuming water density
+
+
+  ## Monoraphidium
+
+  algae_traits_mono <- algae_traits %>%
+    dplyr::filter(species %in% c("Monoraphidium")) %>%
+    mutate(height = geodesic_thickness,
+           biomass=(4/3)*pi*(geodesic_thickness/2)*(height/2)*(geodesic_length/2), # calculate biomass
+           biomass=biomass/10^12)  # change it from um3 to g, assuming water density
+
+  ## not biomass
+
+  algae_traitsNotBiomass <- algae_traits %>%
+    dplyr::filter(!(species %in% flowcam_biomass_species)) %>%
+    mutate(height=as.numeric(NA),
+           biomass=as.numeric(NA))
+
+
+  # join datasets again
+
+  algae_traits <- rbind(algae_traits_normalCases,
+                        algae_traits_2ellipsoids,
+                        algae_traits_2cones,
+                        algae_traitsChlamyClumps,
+                        algae_traitsDesmoClumps,
+                        algae_traits_mono,
+                        algae_traitsNotBiomass) # traits dataset finished here
+
+
+  # calculate biomass per milliliter
+
+  biomasses <- algae_traits %>%
+    group_by(timestamp, bottle, species) %>%
+    summarize(
+      biomass = sum(biomass, na.rm = TRUE),
+      volume_imaged = mean(volume_imaged)
+    ) %>%
+    mutate(
+      biomass = biomass/volume_imaged,
+      measurement = "flowcam",
+      biomass = ifelse(!(species %in% flowcam_biomass_species), NA, biomass)
+    ) %>% # add biomass=NA if not a species
+    select(-volume_imaged)
+
+  densities <- full_join(
+      algae_density,
+      biomasses,
+      by = c("timestamp", "bottle", "species")
+    )
+  densities$biomass[densities$species %in% flowcam_biomass_species & is.na(densities$biomass)] <- 0
+    # mutate(
+    #   biomass = case_when(
+    #     species %in% flowcam_biomass_species & is.na(biomass) ~ 0,
+    #     TRUE ~ biomass)
+    # )
+
+  return(
+    list(
+      traits = algae_traits,
+      density = densities
+    )
+  )
+
+}
