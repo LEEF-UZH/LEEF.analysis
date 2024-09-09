ddef64e85fb7d65223a3661ea0496de742acc7bc
Owen Petchey <owen.petchey@ieu.uzh.ch>
2024-09-09 17:55:53 +0200

    Changed how flowcytometer counts are made
    Before there were two separate gatings, one to get density, and one to get traits. Now there is one gating done to get traits, and density is calculated from the resulting trait data.

--- R/LEEF_2_density_flowcytometer_archive.R
+++ R/LEEF_2_density_flowcytometer_archive.R
@@ -42,86 +42,166 @@ LEEF_2_density_flowcytometer_archive <- function(
     # lapply(
     # pbapply::pblapply(
     pbmcapply::pbmclapply(
+
+      ## do for each timestamp
       timestamps,
       function(timestamp) {
-        datadir <- file.path(
-          extracted_dir,
-          paste0("LEEF.flowcytometer.flowcytometer.", as.character(timestamp))
-        )
 
-        tmpinput <- tempfile()
+        ## before September 2024 the densities were calculated here by reading in the extracted (level1)
+        ## flowcytometer data.
+        ## Now the per bottle density and the per bottle biomass are calculated from the trait data
+        ## that has already been extracted and saved.
 
-        dir.create(file.path(tmpinput, "flowcytometer"), recursive = TRUE, showWarnings = FALSE)
-        file.copy(
-          list.files(datadir, full.names = TRUE),
-          file.path(tmpinput, "flowcytometer"),
-          recursive = TRUE
-        )
+        ## first we need to read in some metadata about the flowcytometer samples
+        flow.data.p1 <- readRDS(file.path(extracted_dir,
+                                          paste0("LEEF.flowcytometer.flowcytometer.",
+                                                 timestamp),
+                                          "flowcytometer_ungated.p_1.rds")) |>
+          select(timestamp, plate, bottle, sample, volume, dilution_factor, date)
+        flow.data.p2 <- readRDS(file.path(extracted_dir,
+                                          paste0("LEEF.flowcytometer.flowcytometer.",
+                                                 timestamp),
+                                          "flowcytometer_ungated.p_2.rds")) |>
+          select(timestamp, plate, bottle, sample, volume, dilution_factor, date)
+        flow.data <- bind_rows(flow.data.p1, flow.data.p2)
 
-        unlink(file.path(tmpinput, "flowcytometer", "gates_coordinates.csv"))
-        write.csv(gates_coordinates, file.path(tmpinput, "flowcytometer", "gates_coordinates.csv"))
-        unlink(file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_1.rds"))
-        LEEF.2.measurement.flowcytometer::calculate_gates(gates_coordinates = gates_coordinates) |>
-          saveRDS(file = file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_1.rds"))
-        unlink(file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_2.rds"))
-        LEEF.2.measurement.flowcytometer::calculate_gates(gates_coordinates = gates_coordinates) |>
-          saveRDS(file = file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_2.rds"))
 
-        message("\n###############################################")
-        message("Gating timestamp ", timestamp, "...")
-
-        suppressMessages({
-          densities <- NULL
-          try(
-            expr = {
-              densities <- LEEF.2.measurement.flowcytometer::extractor_flowcytometer_density(
-                input = tmpinput,
-                output = tmpinput,
-                use_H = use_H,
-                gates_coordinates = gates_coordinates,
-                min_FSC.A = min_FSC.A,
-                dens_back = TRUE
-              )
-            }
+        ## Then read in the trait data
+        bacteria_traits <- readRDS(
+          file.path(
+            output,
+            paste0("flowcytometer_traits_bacteria.", timestamp, ".rds")
           )
-        })
-
-        dir.create(
-          fsa_dir <- file.path(output, "fsa_gated_density", paste0("LEEF.flowcytometer.flowcytometer.", timestamp)),
-          recursive = TRUE,
-          showWarnings = FALSE
         )
-        file.copy(
-          list.files(file.path(tmpinput, "flowcytometer"), "flowcytometer_fsa_ungated.", full.names = TRUE),
-          fsa_dir,
-          recursive = FALSE
-        )
-        file.copy(
-          list.files(file.path(tmpinput, "flowcytometer"), "flowcytometer_gates.", full.names = TRUE),
-          fsa_dir,
-          recursive = FALSE
-        )
-
-        unlink(tmpinput, recursive = TRUE, force = TRUE)
-        if (dir.exists(tmpinput)) {
-          stop("Temporary directory ", tmpinput, " still exists!")
-        }
-
-
-        if (!is.null(densities)) {
-          if (!is.null(particles)) {
-            densities <- densities[densities$species %in% particles, ]
-          }
-
-          message("Saving timestamp ", timestamp, "...")
-
-          saveRDS(
-            object = densities,
-            file = file.path(output, paste0("flowcytometer_density.", timestamp, ".rds"))
+        algae_traits <- readRDS(
+          file.path(
+            output,
+            paste0("flowcytometer_traits_algae.", timestamp, ".rds")
           )
-        } else {
-          message("ERROR in extracting density in timestamp ", timestamp)
-        }
+        )
+
+        ## Then calculate the per sample count and biomass
+        bacteria_summary <- bacteria_traits |>
+          group_by(timestamp, plate, bottle, sample) |>
+          summarise(
+            count = n(),
+            biomass_per_sample = sum(biomass, na.rm = TRUE)
+          ) |>
+          mutate(species = "bacteria")
+
+        algae_summary <- algae_traits |>
+          group_by(timestamp, plate, bottle, sample) |>
+          summarise(
+            count = n(),
+            biomass_per_sample = NA
+          ) |>
+          mutate(species = "algae")
+
+        density_summary <- bind_rows(bacteria_summary, algae_summary)
+
+        ## merge in the metadata
+        density_summary <-
+          left_join(density_summary, flow.data, by = c("timestamp", "plate", "bottle", "sample"))
+
+        ## calcuate the densities of algae and bacteria, in individuals and in grams (biomass)
+        density_summary$density <- density_summary$count * 1000000 /
+          density_summary$volume * density_summary$dilution_factor
+        density_summary$biomass <- density_summary$biomass_per_sample * 1000000 /
+          density_summary$volume * density_summary$dilution_factor
+        density_summary$sample_letter <- substr(x = density_summary$sample, start = 1, stop = 1)
+        density_summary$sample_number <- as.numeric(substr(x = density_summary$sample, start = 2, stop = 3))
+        density_summary$date <- format(as.Date(density_summary$date, "%d-%b-%Y"), "%Y-%m-%d")
+        density_summary <- density_summary[order(density_summary$date,
+                                                 density_summary$sample_letter,
+                                                 density_summary$sample_number), ]
+
+        ## save the density data
+        saveRDS(
+        object = density_summary,
+        file = file.path(output, paste0("flowcytometer_density.x.", timestamp, ".rds"))
+        )
+
+#
+#         ## old code below
+#
+#         datadir <- file.path(
+#           extracted_dir,
+#           paste0("LEEF.flowcytometer.flowcytometer.", as.character(timestamp))
+#         )
+#
+#         tmpinput <- tempfile()
+#
+#         dir.create(file.path(tmpinput, "flowcytometer"), recursive = TRUE, showWarnings = FALSE)
+#         file.copy(
+#           list.files(datadir, full.names = TRUE),
+#           file.path(tmpinput, "flowcytometer"),
+#           recursive = TRUE
+#         )
+#
+#         unlink(file.path(tmpinput, "flowcytometer", "gates_coordinates.csv"))
+#         write.csv(gates_coordinates, file.path(tmpinput, "flowcytometer", "gates_coordinates.csv"))
+#         unlink(file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_1.rds"))
+#         LEEF.2.measurement.flowcytometer::calculate_gates(gates_coordinates = gates_coordinates) |>
+#           saveRDS(file = file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_1.rds"))
+#         unlink(file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_2.rds"))
+#         LEEF.2.measurement.flowcytometer::calculate_gates(gates_coordinates = gates_coordinates) |>
+#           saveRDS(file = file.path(tmpinput, "flowcytometer", "flowcytometer_gates.p_2.rds"))
+#
+#         message("\n###############################################")
+#         message("Gating timestamp ", timestamp, "...")
+#
+#         suppressMessages({
+#           densities <- NULL
+#           try(
+#             expr = {
+#               densities <- LEEF.2.measurement.flowcytometer::extractor_flowcytometer_density(
+#                 input = tmpinput,
+#                 output = tmpinput,
+#                 use_H = use_H,
+#                 gates_coordinates = gates_coordinates,
+#                 min_FSC.A = min_FSC.A,
+#                 dens_back = TRUE
+#               )
+#             }
+#           )
+#         })
+#
+#         dir.create(
+#           fsa_dir <- file.path(output, "fsa_gated_density", paste0("LEEF.flowcytometer.flowcytometer.", timestamp)),
+#           recursive = TRUE,
+#           showWarnings = FALSE
+#         )
+#         file.copy(
+#           list.files(file.path(tmpinput, "flowcytometer"), "flowcytometer_fsa_ungated.", full.names = TRUE),
+#           fsa_dir,
+#           recursive = FALSE
+#         )
+#         file.copy(
+#           list.files(file.path(tmpinput, "flowcytometer"), "flowcytometer_gates.", full.names = TRUE),
+#           fsa_dir,
+#           recursive = FALSE
+#         )
+#
+#         unlink(tmpinput, recursive = TRUE, force = TRUE)
+#         if (dir.exists(tmpinput)) {
+#           stop("Temporary directory ", tmpinput, " still exists!")
+#         }
+#
+#
+#         if (!is.null(densities)) {
+#           if (!is.null(particles)) {
+#             densities <- densities[densities$species %in% particles, ]
+#           }
+#
+#           message("Saving timestamp ", timestamp, "...")
+#
+#           saveRDS(
+#             object = densities,
+#             file = file.path(output, paste0("flowcytometer_density.", timestamp, ".rds"))
+#           )
+#         } else {
+#           message("ERROR in extracting density in timestamp ", timestamp)
+#         }
 
 
         message("Done")
--- R/LEEF_2_extract_traits_flowcytometer_archive.R
+++ R/LEEF_2_extract_traits_flowcytometer_archive.R
@@ -31,7 +31,7 @@
 #' @md
 #' @examples
 LEEF_2_extract_traits_flowcytometer_archive <- function(
-    extracted_dir = "~/Desktop/flowcytometer.FIXED/LEEF.FIXED.archived.data/LEEF/3.archived.data/extracted/", 
+    extracted_dir = "~/Desktop/flowcytometer.FIXED/LEEF.FIXED.archived.data/LEEF/3.archived.data/extracted/",
     gates_coordinates,
     particles = "bacteria",
     timestamps,
@@ -43,171 +43,185 @@ LEEF_2_extract_traits_flowcytometer_archive <- function(
     log10_all = FALSE,
     wellid_keyword = "$WELLID",
     mc.cores = 1) {
-    dir.create(
-        output,
-        showWarnings = FALSE,
-        recursive = TRUE
-    )
+  dir.create(
+    output,
+    showWarnings = FALSE,
+    recursive = TRUE
+  )
 
 
-    # do the stuff -------------------------------------------------------
+  # do the stuff -------------------------------------------------------
 
-    # biomass_per_bottle <- pbapply::pblapply(
-    biomass_per_bottle <- pbmcapply::pbmclapply(
-        timestamps,
-        function(timestamp) {
-            biomass_per_bottle <- dplyr::tibble()
-            datadir <- file.path(
-                extracted_dir,
-                paste0("LEEF.flowcytometer.flowcytometer.", as.character(timestamp))
-            )
-            message("\n###############################################")
-            message("Extracting traits from ", timestamp, "...")
+  # biomass_per_bottle <- pbapply::pblapply(
+  nothing <- pbmcapply::pbmclapply(
 
-            suppressMessages({
-                traits <- NULL
-                try(
-                    expr = {
-                        fsa_p1 <- readRDS(file.path(file.path(datadir, "flowcytometer_fsa_ungated.p_1.rds")))
-                        if (log10_all) {
-                            fsa_p1 <- flowCore::transform(
-                                fsa_p1,
-                                flowCore::transformList(
-                                    c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
-                                    flowCore::truncateTransform("truncate at 1")
-                                )
-                            )
-                            fsa_p1 <- flowCore::transform(
-                                fsa_p1,
-                                flowCore::transformList(
-                                    c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
-                                    "log10"
-                                )
-                            )
-                        }
+    ## do for each timestamp
+    timestamps,
+    function(timestamp) {
 
+      ## not sure what this is created
+      biomass_per_bottle <- dplyr::tibble()
 
-                        fsa_p2 <- readRDS(file.path(file.path(datadir, "flowcytometer_fsa_ungated.p_2.rds")))
-                        if (log10_all) {
-                            fsa_p2 <- flowCore::transform(
-                                fsa_p2,
-                                flowCore::transformList(
-                                    c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
-                                    flowCore::truncateTransform("truncate at 1")
-                                )
-                            )
-                            fsa_p2 <- flowCore::transform(
-                                fsa_p2,
-                                flowCore::transformList(
-                                    c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
-                                    "log10"
-                                )
-                            )
-                        }
+      ## create the directory where the extracted data (level1) is stored
+      datadir <- file.path(
+        extracted_dir,
+        paste0("LEEF.flowcytometer.flowcytometer.", as.character(timestamp))
+      )
+      message("\n###############################################")
+      message("Extracting traits from ", timestamp, "...")
 
+      suppressMessages({
+        traits <- NULL
+        try(
+          expr = {
 
-                        if (is.null(gates_coordinates)) {
-                            gates_coordinates <- utils::read.csv(
-                                file.path(datadir, "gates_coordinates.csv")
-                            )
-                        }
-
-
-                        traits <- LEEF.2.measurement.flowcytometer::extract_traits(
-                            input = datadir,
-                            particles = particles,
-                            metadata_flowcytometer = read.csv(file.path(datadir, "metadata_flowcytometer.csv")),
-                            fsa_p1 = fsa_p1,
-                            fsa_p2 = fsa_p2,
-                            gates_coordinates = gates_coordinates,
-                            min_FSC.A = min_FSC.A,
-                            use_H = use_H,
-                            wellid_keyword = wellid_keyword
-                        )
-                    }
+            ## read plate 1 data from extracted (level1) data
+            fsa_p1 <- readRDS(file.path(file.path(datadir, "flowcytometer_fsa_ungated.p_1.rds")))
+            if (log10_all) {
+              fsa_p1 <- flowCore::transform(
+                fsa_p1,
+                flowCore::transformList(
+                  c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
+                  flowCore::truncateTransform("truncate at 1")
                 )
-            })
-
-            if (!is.null(traits)) {
-                dir.create(file.path(output), showWarnings = FALSE)
-                biomass_per_bottle <- dplyr::tibble()
-                dens_fn <- file.path(output, paste0("flowcytometer_density.", timestamp, ".rds"))
-                dens <- readRDS(dens_fn)
-
-                for (p in particles) {
-                    message("\nExtracting ", p, "...")
-                    
-                    traits[[p]]$species <- p
-                    traits[[p]]$length <- as.numeric(NA)
-                    traits[[p]]$volume <- as.numeric(NA)
-                    traits[[p]]$biomass <- as.numeric(NA)
-
-                    if (p == "bacteria") {
-                        # stop("There is something still wrong here!!!")
-
-                        traits[[p]]$length <- traits[[p]]$FSC.A * length_slope + length_intercept
-
-                        # traits[[p]]$length[traits[[p]]$length <= 0] <- as.numeric(NA)
-                        ##
-                        ## We assume the bacteria to have the shape of an ellipsoid of revolution
-                        ## See e.g. https://en.wikipedia.org/wiki/Ellipsoid#Volume
-                        ## The volume can be calculated as follows
-                        ## V = 4/3 * pi * a * b * c ## we assume, that :
-                        ## a = length / 2 ## b = c = a/3 = length / 6
-                        ## therefore we have:
-                        ## V = 4/3 * pi * (l/2) * (l/6) * (l/6)
-                        ## V = 4/3 * pi * l^3 / 72
-                        ##
-
-                        traits[[p]]$volume <- 4 / 3 * pi * traits[[p]]$length^3 / 72
-                        traits[[p]]$biomass <- traits[[p]]$volume / 10^12
-                    }
-
-                    saveRDS(
-                        object = traits[[p]],
-                        file = file.path(output, paste0("flowcytometer_traits_", p, ".", timestamp, ".rds"))
-                    )
-
-                    bm <- traits[[p]] |>
-                        group_by(bottle, sample, plate, species) |>
-                        summarise(biomass = sum(biomass)) |>
-                        left_join(
-                            x = dens,
-                            by = dplyr::join_by(bottle, sample, plate, species)
-                        ) |>
-                        mutate(biomass = biomass * 1000000 / volume * dilution_factor) |>
-                        ungroup() |>
-                        filter(species == p)
-
-                    biomass_per_bottle <- rbind(
-                        biomass_per_bottle,
-                        bm
-                    )
-
-                    rm(bm)
-                    message("Done\n")
-                }
-
-                unlink(dens_fn)
-                saveRDS(
-                    object = biomass_per_bottle,
-                    file = dens_fn
+              )
+              fsa_p1 <- flowCore::transform(
+                fsa_p1,
+                flowCore::transformList(
+                  c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
+                  "log10"
                 )
-            } else {
-                message("ERROR in extracting traits in timestamp ", timestamp)
+              )
             }
 
-            message("Saving Densities")
+            ## read plate 2 data from extracted (level1) data
+            fsa_p2 <- readRDS(file.path(file.path(datadir, "flowcytometer_fsa_ungated.p_2.rds")))
+            if (log10_all) {
+              fsa_p2 <- flowCore::transform(
+                fsa_p2,
+                flowCore::transformList(
+                  c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
+                  flowCore::truncateTransform("truncate at 1")
+                )
+              )
+              fsa_p2 <- flowCore::transform(
+                fsa_p2,
+                flowCore::transformList(
+                  c("FL2-A", "FL1-H", "FL2-H", "FL3-H", "FL4-H", "FSC-H", "SSC-H"),
+                  "log10"
+                )
+              )
+            }
 
-            message("Done")
-            message("###############################################")
+            ## read in the gates if they aren't given as an argument
+            if (is.null(gates_coordinates)) {
+              gates_coordinates <- utils::read.csv(
+                file.path(datadir, "gates_coordinates.csv")
+              )
+            }
 
 
-            return(biomass_per_bottle)
-        },
-        mc.preschedule = FALSE,
-        mc.cores = mc.cores
-    )
-    names(biomass_per_bottle) <- timestamps
-    invisible(biomass_per_bottle)
+            traits <- LEEF.2.measurement.flowcytometer::extract_traits(
+              input = datadir,
+              particles = particles,
+              metadata_flowcytometer = read.csv(file.path(datadir, "metadata_flowcytometer.csv")),
+              fsa_p1 = fsa_p1,
+              fsa_p2 = fsa_p2,
+              gates_coordinates = gates_coordinates,
+              min_FSC.A = min_FSC.A,
+              use_H = use_H,
+              wellid_keyword = wellid_keyword
+            )
+          }
+        )
+      })
+
+      if (!is.null(traits)) {
+        dir.create(file.path(output), showWarnings = FALSE)
+
+        ## The next three lines were from when the density data had been already calculated, and the biomass data
+        ## was calculated from the density data. This is not the case anymore, so I (OP) commented them out.
+        #biomass_per_bottle <- dplyr::tibble()
+        #dens_fn <- file.path(output, paste0("flowcytometer_density.", timestamp, ".rds"))
+        #dens <- readRDS(dens_fn)
+
+        for (p in particles) {
+          message("\nExtracting ", p, "...")
+
+          traits[[p]]$species <- p
+          traits[[p]]$length <- as.numeric(NA)
+          traits[[p]]$volume <- as.numeric(NA)
+          traits[[p]]$biomass <- as.numeric(NA)
+
+          ## if working on bacteria particles then add a biomass column
+          if (p == "bacteria") {
+            # stop("There is something still wrong here!!!")
+
+            traits[[p]]$length <- traits[[p]]$FSC.A * length_slope + length_intercept
+
+            # traits[[p]]$length[traits[[p]]$length <= 0] <- as.numeric(NA)
+            ##
+            ## We assume the bacteria to have the shape of an ellipsoid of revolution
+            ## See e.g. https://en.wikipedia.org/wiki/Ellipsoid#Volume
+            ## The volume can be calculated as follows
+            ## V = 4/3 * pi * a * b * c ## we assume, that :
+            ## a = length / 2 ## b = c = a/3 = length / 6
+            ## therefore we have:
+            ## V = 4/3 * pi * (l/2) * (l/6) * (l/6)
+            ## V = 4/3 * pi * l^3 / 72
+            ##
+
+            traits[[p]]$volume <- 4 / 3 * pi * traits[[p]]$length^3 / 72
+            traits[[p]]$biomass <- traits[[p]]$volume / 10^12
+          }
+
+          saveRDS(
+            object = traits[[p]],
+            file = file.path(output, paste0("flowcytometer_traits_", p, ".", timestamp, ".rds"))
+          )
+
+          ## not calculating bottle biomass here anymore
+          # bm <- traits[[p]] |>
+          #   group_by(bottle, sample, plate, species) |>
+          #   summarise(biomass = sum(biomass)) |>
+          #   left_join(
+          #     x = dens,
+          #     by = dplyr::join_by(bottle, sample, plate, species)
+          #   ) |>
+          #   mutate(biomass = biomass * 1000000 / volume * dilution_factor) |>
+          #   ungroup() |>
+          #   filter(species == p)
+          #
+          # biomass_per_bottle <- rbind(
+          #   biomass_per_bottle,
+          #   bm
+          # )
+
+          # rm(bm)
+          # message("Done\n")
+        }
+
+        # unlink(dens_fn)
+        # saveRDS(
+        #  object = biomass_per_bottle,
+        #  file = dens_fn
+        # )
+      } else {
+        message("ERROR in extracting traits in timestamp ", timestamp)
+      }
+
+      ##message("Saving Densities")
+
+      message("Done")
+      message("###############################################")
+
+
+      #return(biomass_per_bottle)
+      return(NULL)
+    },
+    mc.preschedule = FALSE,
+    mc.cores = mc.cores
+  )
+  #names(biomass_per_bottle) <- timestamps
+  invisible(nothing)
 }
